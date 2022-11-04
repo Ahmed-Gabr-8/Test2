2bb4a06c4e8b263b855a05be43196b1a6e280dc7
Ahmed Gabr <es_Ahmed.Gabr2025@alexu.edu.eg>
2022-11-04 21:03:06 +0200

    Added Run Exception

--- TestGitHub2.java
+++ TestGitHub2.java
@@ -4,10 +4,25 @@ package testgithub2;
 public class TestGitHub2 {
 
   
-    public static void main(String[] args) {
+    public static void main(String[] args){
+     
+//        try{
+//         foo(-1);
+//                 }
+//        catch(RunExc re){
+//            System.out.println("RE detected");
+//        }
+
+          foo(-1);
+     
         
-        
-        
+    }
+    
+    public static void foo(int x) throws RunExc{
+        if(x < 0){
+            throw new RunExc("X < 0");
+        }
+    
     }
     
 }
