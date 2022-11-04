a4317c46769aec2c0725a65580828ef62cf15497
Ahmed Gabr <es_Ahmed.Gabr2025@alexu.edu.eg>
2022-11-04 21:13:24 +0200

    Added Checked Exception

--- TestGitHub2.java
+++ TestGitHub2.java
@@ -3,11 +3,20 @@ package testgithub2;
 
 public class TestGitHub2 {
 
-  
+ 
     public static void main(String[] args) {
-        
-        
-        
+        try{
+        foo(5);}
+        catch(CheckExc ce){
+            System.out.println("Checked exception");
+        }
+    }
+    
+    public static void foo(int x) throws CheckExc{
+        if(x<0){
+            throw new CheckExc("Error");
+        }
+    
     }
     
 }
